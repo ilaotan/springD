19192392bba5967ad338d3f8c35782f4b7432237
ilaotan <tanliansheng2011@gmail.com>
2016-01-04 23:24:57 +0800

    1.4 生成用户密码

---
+++ src/test/java/com/springD/user/Generate.java
@@ -0,0 +1,26 @@
+package com.springD.user;
+
+import com.springD.framework.encoder.MD5Crypt;
+import com.springD.framework.encoder.Md5PwdEncoder;
+
+/**
+ * Created by tan on 2016/1/4.
+ */
+public class Generate {
+
+    public static void main(String[] args) {
+
+        String salt1 = "r4QD4vG9";
+
+        String passwd1 = Md5PwdEncoder.encodePassword(salt1+"tanlsh","");
+
+        String passwd2 = Md5PwdEncoder.encodePassword(passwd1,"");
+
+        String radomSalt = "b123";
+
+        String finalPwd = Md5PwdEncoder.encodePassword(radomSalt+passwd2,"");
+
+        System.out.print(finalPwd);
+
+    }
+}
