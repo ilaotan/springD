321886b929d7aa2e4363b679fe69df33d6345eb5
ilaotan <tanliansheng2011@gmail.com>
2016-03-06 12:47:11 +0800

    暂时提交

--- src/main/java/com/springD/application/front/controller/IndexController.java
+++ src/main/java/com/springD/application/front/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.springD.application.front.controller;
 
 import com.springD.framework.web.BaseController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,7 +18,9 @@ public class IndexController extends BaseController {
 
 
     @RequestMapping(value = "/front/index",method = RequestMethod.GET)
-    public String index(HttpServletRequest request, HttpServletResponse response){
+    public String index(HttpServletRequest request, HttpServletResponse response,Model model){
+        String x = "test111233311";
+        model.addAttribute("name",x);
         return "front/index";
     }
 }
--- src/main/webapp/WEB-INF/jsp/front/index.jsp
+++ src/main/webapp/WEB-INF/jsp/front/index.jsp
@@ -2,16 +2,17 @@
 <%@ include file="/WEB-INF/common/taglibs.jsp" %>
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=EDGE" />
-<title></title>
-<%@ include file="/WEB-INF/common/assets.jsp"%>
-<script type="text/javascript">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=EDGE" />
+		<title></title>
+		<%@ include file="/WEB-INF/common/assets.jsp"%>
+		<script type="text/javascript">
 
-</script>
-</head>
-<body >
-	<h2>it is work  this is front page </h2>
-</body>
+		</script>
+	</head>
+	<body >
+		<h2>it is work  this is front page </h2>
+		${name}
+	</body>
 </html>
