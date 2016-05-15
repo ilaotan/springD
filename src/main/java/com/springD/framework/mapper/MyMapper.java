5e728e3a6230c923bbbb3065b218349d7d374634
ilaotan <tanliansheng2011@gmail.com>
2016-05-15 11:57:30 +0800

    shiro的缓存改为redis

--- src/main/java/com/springD/framework/mapper/MyMapper.java
+++ src/main/java/com/springD/framework/mapper/MyMapper.java
@@ -1,7 +1,12 @@
 package com.springD.framework.mapper;
 
 import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.Marker;
 import tk.mybatis.mapper.common.MySqlMapper;
+import tk.mybatis.mapper.common.base.BaseInsertMapper;
+import tk.mybatis.mapper.common.base.BaseSelectMapper;
+import tk.mybatis.mapper.common.base.BaseUpdateMapper;
+import tk.mybatis.mapper.common.base.delete.DeleteByPrimaryKeyMapper;
 
 /**
  * 继承自己的MyMapper
@@ -9,6 +14,7 @@ import tk.mybatis.mapper.common.MySqlMapper;
  * @author liuzh_3nofxnp
  * @since 2015-09-06 21:53
  */
-public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
+public interface MyMapper<T> extends
+        BaseSelectMapper<T>, BaseInsertMapper<T>, BaseUpdateMapper<T>,DeleteByPrimaryKeyMapper<T>,Marker,MySqlMapper<T>  {
 
 }
---
+++ src/main/java/com/springD/framework/shiro/redisCache/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * <p>Description: description
+ * <p>User: tanliansheng
+ * <p>Date: 2016/5/14  23:16
+ * <p>Version: 1.0
+ */
+package com.springD.framework.shiro.redisCache;
+
+
+/*
+*
+*    shiro-redis缓存的实现
+*
+* */
